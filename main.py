0f82a49a70a1b7018ce4aa8c1995f7755d316c06
Anum Afzal <anumafzaldogar@gmail.com>
2024-10-10 15:58:34 +0200

    ..

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import xgboost as xgb
 from sklearn.metrics import mean_squared_error
 from dotenv import load_dotenv
 warnings.filterwarnings("ignore")
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from features.features import construct_training_corpus
 
 if __name__ == '__main__':
@@ -31,11 +32,12 @@ if __name__ == '__main__':
     #print (diamonds.describe())
     #diamonds.to_excel("Diamonds.xlsx")
     diamonds = pd.read_excel("Diamonds.xlsx")
-    print(diamonds.describe())
+
     diamonds.drop('y_weighted_target',axis=1)
     diamonds.drop('target', axis=1)
     diamonds.drop('source', axis=1)
     diamonds.drop('learning_difficult', axis=1)
+    print(diamonds.describe())
 
 
 
@@ -47,7 +49,7 @@ if __name__ == '__main__':
     # Convert to Pandas category
     for col in cats:
         X[col] = X[col].astype('category')
-    print (X.dtypes)
+    #print (X.dtypes)
 
     # Split the data
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
@@ -73,7 +75,16 @@ if __name__ == '__main__':
 
 
     preds = model.predict(dtest_reg)
-    rmse = mean_squared_error(y_test, preds, squared=False)
+    rmse = mean_squared_error(y_test, preds, squared=True)
     print(f"RMSE of the base model: {rmse:.3f}")
+
+    # Step 5: Evaluate the model's accuracy on the test set
+    mse = mean_squared_error(y_test, preds)
+    mae = mean_absolute_error(y_test, preds)
+    r2 = r2_score(y_test, preds)
+
+    print(f"Mean Squared Error: {mse:.2f}")
+    print(f"Mean Absolute Error: {mae:.2f}")
+    print(f"R^2 Score: {r2:.2f}")
 
 
