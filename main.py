0a14fe42ae2a1383c19e1c4ee9794d4e4201ba40
Anum Afzal <anumafzaldogar@gmail.com>
2024-11-22 15:20:41 +0100

    .

--- main.py
+++ main.py
@@ -341,7 +341,7 @@ if __name__ == '__main__':
         features_norm = normalize_features(features)
         scores_all_norm = run_regression(features_norm, mode='all-norm')
         file_name_norm = f"training_features_ds_{n}_llama3.1_8b_{experiment}_samples{num_samples}_norm.xlsx"
-        if os.path.isfile(file_name) == False:
+        if os.path.isfile(file_name_norm) == False:
             scores_all_norm.to_excel(file_name_norm)
 
         pd_scores = pd.DataFrame.from_records([scores_baseline_raw, scores_baseline_norm, scores_all_raw, scores_all_norm])
