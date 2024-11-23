fixed bug with code normalization

main.py
@@ -39,6 +39,11 @@ features_to_normalize = {'source': ['source_bert_precision', 'source_bert_recall
                          'target': ['target_vocab_overlap', 'target_Relevance', 'target_Coherence', 'target_Consistency',
                                     'target_Fluency', 'target_bert_precision', 'target_bert_recall', 'target_bert_f1']
                          }
+normalize_range = {'bert': [-1, 1], 'overlap': [0, 100], 'entropy': [0,100], 'learning_difficult': [0, 100],
+                   'kl-divergence': [0, 100], 'js-divergence': [0, 100], 'Fluency': [1,3],
+                   'Relevance': [1,5], 'Coherence': [1,5], 'Consistency': [1,5]
+
+}
 reduced_features_target = {
     'target_bert': ['target_bert_f1'],
     'target_rouge': ['target_rouge1', 'target_rouge2', 'target_rougeL'],
@@ -216,14 +221,15 @@ def derive_baseline_features(df):
     '''
 
     return df
-def NormalizeData(data):
-    return (data - np.min(data)) / (np.max(data) - np.min(data))
+
 def normalize_features(df):
     def norm(df, features_to_normalize, update_y = None, a = None):
         for feature in features_to_normalize:
             if feature in df.columns:
                 numbers = np.array(df[feature]).reshape((-1,1))
-                df[feature] = NormalizeData(numbers)
+                print (feature)
+                old_range_key = [key for key in normalize_range.keys() if key in feature][0]
+                df[feature] = np.interp(numbers, normalize_range[old_range_key], [0,1])
         weighted_y_col = []
         if a is not None:
             for col in df.columns:
@@ -284,8 +290,8 @@ def run_regression(df:pd.DataFrame, mode:str):
     # Split the dataset into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,)
     print ("Predictions with XGBoost")
-    xgboost_scores =  xgboost(X_train, X_test, y_train, y_test)
-    #xgboost_scores = {'xgboost-mse': 0, 'xgboost-mae': 0, "xgboost-rmse": 0, "xgboost-r2":0}
+    #xgboost_scores =  xgboost(X_train, X_test, y_train, y_test)
+    xgboost_scores = {'xgboost-mse': 0, 'xgboost-mae': 0, "xgboost-rmse": 0, "xgboost-r2":0}
 
     print("Predictions with Linear Regression")
     reg_scores = linear_regression(X_train, X_test, y_train, y_test)