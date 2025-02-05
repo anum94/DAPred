b2113bd14cf6bc8a484c02e48e8da769df9e5020
Anum Afzal <anumafzaldogar@gmail.com>
2025-02-05 11:43:35 +0100

    ..

--- main.py
+++ main.py
@@ -408,9 +408,9 @@ def run_regression(df:pd.DataFrame, mode:str, feature_selection_bool:bool, acros
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33,)
 
     print ("Predictions with XGBoost")
-    #xgboost_scores =  xgboost(X_train, X_test, y_train, y_test)
-    xgboost_scores = {'xgboost-mse': 0, 'xgboost-mae': 0, "xgboost-rmse": 0, "xgboost-r2":0,
-                      'xgboost-k-fold-mse': 0, 'xgboost-k-fold-mae': 0, "xgboost-k-fold-rmse": 0, "xgboost-k-fold-r2":0}
+    xgboost_scores =  xgboost(X_train, X_test, y_train, y_test)
+    #xgboost_scores = {'xgboost-mse': 0, 'xgboost-mae': 0, "xgboost-rmse": 0, "xgboost-r2":0,
+    #                  'xgboost-k-fold-mse': 0, 'xgboost-k-fold-mae': 0, "xgboost-k-fold-rmse": 0, "xgboost-k-fold-r2":0}
 
     print("Predictions with Linear Regression")
     reg_scores = linear_regression(X_train, X_test, y_train, y_test)
